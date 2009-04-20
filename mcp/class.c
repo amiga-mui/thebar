094296b4007aa4e93bd4d1cc09df39ea1d1d0ef7
Jens Maus <mail@jens-maus.de>
2009-04-20 18:24:07 +0000

    - added missing $Id$ strings and changed all rev.h files to   the new version.h layout.

--- mcp/class.c
+++ mcp/class.c
@@ -2,7 +2,7 @@
 
  TheBar.mcc - Next Generation Toolbar MUI Custom Class
  Copyright (C) 2003-2005 Alfonso Ranieri
- Copyright (C) 2005-2007 by TheBar.mcc Open Source Team
+ Copyright (C) 2005-2009 by TheBar.mcc Open Source Team
 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
@@ -16,13 +16,15 @@
 
  TheBar class Support Site:  http://www.sf.net/projects/thebar
 
+ $Id$
+
 ***************************************************************************/
 
 #include "class.h"
 #include <proto/iffparse.h>
 #include <prefs/prefhdr.h>
 #include <libraries/asl.h>
-#include "rev.h"
+#include "version.h"
 #include "private.h"
 
 /***********************************************************************/
