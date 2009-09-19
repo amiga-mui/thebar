d77527ec7b805275c2e98df21886faa3d06bbb9b
Thore Böckelmann <tboeckel@gmx.de>
2009-09-19 16:35:32 +0000

    * misc: replaced the per-instance memory pool by a global and semaphore   protected one. There is absolutely no benefit in having a separate pool   for every instance with only just one allocation within this pool.

--- mcc/class_protos.h
+++ mcc/class_protos.h
@@ -28,9 +28,11 @@ IPTR DoSuperNew(struct IClass *cl, Object *obj, IPTR tag1, ...);
 #else
 Object * VARARGS68K DoSuperNew(struct IClass *cl, Object *obj, ...);
 #endif
-APTR allocVecPooled(APTR pool, ULONG size);
-void freeVecPooled (APTR pool, APTR mem);
-APTR reallocVecPooledNC(APTR pool,APTR mem,ULONG size);
+BOOL myCreatePool(void);
+void myDeletePool(void);
+APTR allocVecPooled(ULONG size);
+void freeVecPooled(APTR mem);
+APTR reallocVecPooledNC(APTR mem, ULONG size);
 
 /* brc1.c */
 int BRCUnpack(APTR pSource, APTR pDest, LONG srcBytes0, LONG dstBytes0);
