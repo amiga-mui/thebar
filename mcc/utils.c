0b91121e0c241aa864f1218eed1280903f09bae7
Thore Böckelmann <tboeckel@gmx.de>
2012-10-09 08:40:08 +0000

    * mcc/utils.c, mcc/button/utils.c: reverted the AROS specific modification of DoSuperNew() to the previous state. The last change done by Matthias Rustler crashes AROS distributions like Icaros so badly that even VirtualBox shuts down the virtual machine.

--- mcc/utils.c
+++ mcc/utils.c
@@ -27,29 +27,21 @@
 // DoSuperNew()
 // Calls parent NEW method within a subclass
 #if !defined(__MORPHOS__)
-#ifdef __AROS__
-Object * VARARGS68K DoSuperNew(struct IClass *cl, Object *obj, Tag tag1, ...)
-{
-    AROS_SLOWSTACKTAGS_PRE_AS(tag1, Object *)
-    retval = (Object *)DoSuperMethod(cl, obj, OM_NEW, AROS_SLOWSTACKTAGS_ARG(tag1), NULL);
-    AROS_SLOWSTACKTAGS_POST
-}
-#else
 Object * VARARGS68K DoSuperNew(struct IClass *cl, Object *obj, ...)
 {
   Object *rc;
   VA_LIST args;
 
-  ENTER();
-
   VA_START(args, obj);
+  #if defined(__AROS__)
+  rc = (Object *)DoSuperNewTagList(cl, obj, NULL, (struct TagItem *)VA_ARG(args, IPTR));
+  #else
   rc = (Object *)DoSuperMethod(cl, obj, OM_NEW, VA_ARG(args, ULONG), NULL);
+  #endif
   VA_END(args);
 
-  RETURN(rc);
   return rc;
 }
-#endif
 #endif
 
 /***********************************************************************/
