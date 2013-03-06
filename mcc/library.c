a06033fae7c4e31b75427053ca60d0bdd0fc1b4d
Thore Böckelmann <tboeckel@gmx.de>
2013-03-06 08:27:16 +0000

    * include/mccinit.c, include/shellstart.c: rearranged the code to ensure that trying to start the final class from a shell does not crash.

--- mcc/library.c
+++ mcc/library.c
@@ -26,6 +26,10 @@
 #include "version.h"
 
 /******************************************************************************/
+/* include the minimal startup code to be able to start the class from a      */
+/* shell without crashing the system                                          */
+/******************************************************************************/
+#include "shellstart.c"
 
 #define VERSION       LIB_VERSION
 #define REVISION      LIB_REVISION
--- mcp/library.c
+++ mcp/library.c
@@ -26,6 +26,10 @@
 #include <mcc_common.h>
 
 /******************************************************************************/
+/* include the minimal startup code to be able to start the class from a      */
+/* shell without crashing the system                                          */
+/******************************************************************************/
+#include "shellstart.c"
 
 #define VERSION       LIB_VERSION
 #define REVISION      LIB_REVISION
